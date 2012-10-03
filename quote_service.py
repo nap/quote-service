05273b9ac88faf251d70fff24b9d38a157178061
nap <jean.bernard.ratte@gmail.com>
2012-10-03 15:36:45 -0400

    Added some comment in the source code.

--- quote_service.py
+++ quote_service.py
@@ -16,9 +16,11 @@ DEFAULT_PORT = 8080
 DEFAULT_HOST = ''
 MAXIMUM_CONNEXIONS = 5000
 
+# Response to all HTTP request with 501 status code except get GET 
 _bad_request = "HTTP/1.1 501 Not Implemented\r\n" \
                "Server: quote-service/0.1.1\r\n\r\n"
 
+# Response to all GET request with a 200 OK status code
 _ok_request = "HTTP/1.1 200 Connection established\r\n" \
               "Connection: keep-alive\r\n" \
               "Content-Length: %s bytes\r\n" \
@@ -27,6 +29,9 @@ _ok_request = "HTTP/1.1 200 Connection established\r\n" \
 
 
 def get_socket(host, port, max_connections):
+    """
+    Create a listening socket to accept client connections and set it as non-blocking.
+    """
     sock = socket.socket(socket.AF_INET)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setblocking(0)
@@ -36,6 +41,10 @@ def get_socket(host, port, max_connections):
 
 
 def connection_ready(sok, fd, events):
+    """
+    On a read event, we accept the connection and wrap it in a IOStream 
+    and make sure that the socket is non-blocking.
+    """
     # Set the file descriptor of the current socket as non-blocking to avoid EAGAIN error.
     flags = fcntl.fcntl(fd, fcntl.F_GETFL)
     fcntl.fcntl(fd, fcntl.F_SETFL, flags & ~O_NONBLOCK)
@@ -58,6 +67,10 @@ def connection_ready(sok, fd, events):
 
 
 def send_quote(client, message):
+    """
+    Verify that the client sent a GET message then send a random quote
+    to finally close the connection when the quote is fully sent.
+    """
     if message[:14] == 'GET / HTTP/1.1':
         quote = json.dumps(quotes[randint(1, len(quotes))])
         client.write(_ok_request % (len(quote), quote), callback=client.close)
@@ -87,11 +100,14 @@ if __name__ == '__main__':
                 raise ValueError('Invalid Port number or Maximum Connection number error.')
 
         with codecs.open(INPUT, 'r', 'utf-8') as json_file:
+            # Load all the quotes in memory
             quotes = json.loads(''.join(json_file.readlines()))
 
             sok = get_socket(DEFAULT_HOST, DEFAULT_PORT, MAXIMUM_CONNEXIONS)
             callback = functools.partial(connection_ready, sok)
             io_loop = ioloop.IOLoop.instance()
+            
+            # Get socket and wait for connections
             io_loop.add_handler(sok.fileno(), callback, io_loop.READ)
 
             # Launch the io loop.
